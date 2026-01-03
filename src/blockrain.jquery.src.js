8c65a610d4151e224ceeed7b5a426666c5cd5e99
Timothy Prinz <tprinz@Timothys-MacBook-Air.local>
2026-01-02 20:06:03 -0800

    vibe coding modifications

--- src/blockrain.jquery.src.js
+++ src/blockrain.jquery.src.js
@@ -52,6 +52,53 @@
       this.showGameOverMessage();
       this._board.gameover = true;
       this.options.onGameOver.call(this.element, this._filled.score);
+      // This is the javascript in your Squarespace page for blockrain.js
+
+      async function submitScore(playerName, scoreValue) {
+        // ❗️ Replace this with the Function URL you copied from AWS Lambda
+        const LAMBDA_PROXY_URL = "https://tehoesh2ofabsx33fclbp7culy0fyigt.lambda-url.us-east-2.on.aws/";
+
+        const scoreData = {
+          player: playerName,
+          score: this._filled.score
+        };
+
+        try {
+          const response = await fetch(LAMBDA_PROXY_URL, {
+            method: "POST",
+            // Notice: No 'Authorization' header here! It's handled securely in the Lambda.
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(scoreData)
+          });
+
+          const result = await response.json();
+
+        if (!response.ok) {
+            // This will catch errors sent back from your Lambda function
+            alert(`Error submitting score: ${result.error}`);
+            return;
+        }
+        
+        console.log('Success:', result);
+        alert('Your score has been submitted!');
+
+      } catch (error) {
+        console.error('Network error or failed to fetch:', error);
+        alert('Could not submit score. Please check your connection.');
+      }
+    }
+
+// You would call this function when the game is over.
+// For example, from within blockrain.js's onGameOver event:
+// onGameOver: function(score) {
+//   const playerName = prompt("Game Over! Enter your name:", "Player");
+//   if (playerName) {
+//     submitScore(playerName, score);
+//   }
+// }
+
     },
 
     _doStart: function() {
@@ -1454,7 +1501,15 @@
       var rotateRight = function() {
         game._board.cur.rotate('right'); 
       }
-
+      var hardDrop = function() {
+        if( ! game._board.cur ) { return; }
+        while (!game._checkCollisions(game._board.cur.x, game._board.cur.y + 1, game._board.cur.getBlocks())) {
+            game._board.cur.y++;
+        }
+        game._board.dropCount = game._board.dropDelay;
+        game._board.animate();
+        game._board.renderChanged = true;
+      }
       // Handlers: These are used to be able to bind/unbind controls
       var handleKeyDown = function(evt) {
         if( ! game._board.cur ) { return true; }
@@ -1476,6 +1531,7 @@
           case 38: /*up*/     game._board.cur.rotate('right'); break;
           case 88: /*x*/      game._board.cur.rotate('right'); break;
           case 90: /*z*/      game._board.cur.rotate('left'); break;
+          case 32: /*space*/  hardDrop(); break;
           default: caught = false;
         }
         if (caught) evt.preventDefault();
